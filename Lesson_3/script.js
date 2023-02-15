312709dfe38009e10907327bf06390b140250cf1
257 <a257@Zhenia-MacBook-Pro.local>
2023-02-15 11:13:29 +0200

    add task3 lesson3

--- Lesson_3/script.js
+++ Lesson_3/script.js
@@ -68,10 +68,37 @@ function balanceCalc() {
     return (document.querySelector('.result2').innerHTML = `
     <span class="green-color-result"> Вартість товару ${itemPrice.toFixed(
       2
-    )}, на рахунку ${cashBalance.toFixed(2)}. Покупка дозволена.   
+    )}, на рахунку ${cashBalance.toFixed(2)}. Покупка дозволена.</span>  
     `)
   }
 }
 
 // ================== Задача 3 ==================
-// З клавіатури вводиться ціна товару і кількість грошей. Якщо грошей не вистачає то відмовляємо у покупці, інакше, якщо ще залишаються гроші, то пропонуємо купити лотерею за 4 грн.
+// Випадковим чином генерується число від 1 до 5. Спробуйте вгадати число за 2 спроби.
+
+// формула рандомного числа min + Math.floor(Math.random() * (max - min + 1))
+
+document.querySelector('.button3').addEventListener('click', guessNaumer)
+
+function guessNaumer() {
+  const MIN = 1
+  const MAX = 5
+  // отримуємо дані від користувача
+  const attempt1 = parseInt(document.querySelector('.getAttempt1').value)
+  const attempt2 = parseInt(document.querySelector('.getAttempt2').value)
+  // генерація рандомного чмсла
+  const randomNum = MIN + Math.floor(Math.random() * (MAX - MIN + 1))
+  console.log(randomNum)
+  // якщо перша або друга спроба успішні
+  if (attempt1 === randomNum || attempt2 === randomNum) {
+    return (document.querySelector('.result3').innerHTML = `
+    <span class="green-color-result"> 
+       Ви вгадали. Загадане число: ${randomNum}
+    </span>
+    `)
+  }
+  // інакше повертаємо інфо про невдачу
+  return (document.querySelector('.result3').innerHTML = `
+       Ви не вгадали. Загадане число: ${randomNum}
+    `)
+}
