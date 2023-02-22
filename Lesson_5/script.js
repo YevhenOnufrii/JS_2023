1ccac8592b57f2a0fd57aa1412c6c7deedc2d84f
257 <a257@Zhenia-MacBook-Pro.local>
2023-02-22 12:26:03 +0200

    add task4 HW#5

--- Lesson_5/script.js
+++ Lesson_5/script.js
@@ -1,4 +1,7 @@
 // Homework #5
+
+// ==================== Task 1 ====================
+
 // ? Задача 1. Вивести з використанням циклів маркований список з випадковими числами (1-100). Кількість випадкових чисел вводиться користувачем.
 
 function markingList() {
@@ -20,6 +23,8 @@ function markingList() {
     list.appendChild(listItem) // монтуємо елемент в список
   }
 }
+
+// ==================== Task 2 ====================
 
 // ? Задача 2. Створити 10 елементів для введення цін продуктів
 
@@ -46,10 +51,13 @@ function listForEnteringProdPrices() {
   }
 }
 
+// ==================== Task 3 ====================
+
 // ? Задача 3. Вивести таблицю з одним рядком і  7 стовпцями.
 
 function getTableWithSevenColums() {
   const columnsAmount = 7
+  // div у який виводиться результат
   let resDiv = document.querySelector('.result3')
   resDiv.innerHTML = ''
   let i = 1
@@ -58,5 +66,35 @@ function getTableWithSevenColums() {
     tableCol.textContent = `${i}`
     resDiv.appendChild(tableCol)
     i++
+  }
+}
+
+// ==================== Task 4 ====================
+
+//Задача 4. Вивести таблицю з 3 рядків і 7 стовпців
+
+function getTableWithSevenColumnsThreeRows() {
+  // кількість колонок і рядків
+  const columnsAmount = 7
+  const rowsAmount = 3
+
+  // таблиця для виведення результату
+  const resTable = document.querySelector('.result4').querySelector('table')
+  // зачистка операційного поля
+  resTable.innerHTML = ''
+
+  for (let i = 1; i <= rowsAmount; i++) {
+    // створюємо рядок
+    const row = document.createElement('tr')
+    // додаємо його до таблиці
+    resTable.appendChild(row)
+    for (let j = 1; j <= columnsAmount; j++) {
+      // створюємо ячейку
+      const column = document.createElement('td')
+      // додаємо текст в ячейку
+      column.textContent = `${j}`
+      // монтуємо ячейку до рядка
+      row.appendChild(column)
+    }
   }
 }
