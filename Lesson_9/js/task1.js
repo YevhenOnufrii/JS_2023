fb71b0601753b3724c706b0a1aa7980325637219
Yevhen <onufrii.socnet@gmail.com>
2023-03-07 22:30:24 +0200

    add task_1 and task_2 HW#9

--- Lesson_9/js/task1.js
+++ Lesson_9/js/task1.js
@@ -1,1 +1,10 @@
+//================ Задача 1
 
+// Користувач вводить кількість елементів. Створити масив, що складається з вказаної кількості елементів заповнених нулями.
+const fillArray = () => {
+  const elementNumber = parseInt(document.getElementById('task1').value)
+  if (elementNumber)
+    document.querySelector('.result_1').innerHTML = `
+   [ ${new Array(elementNumber).fill(0)} ]
+  `
+}
---
+++ Lesson_9/js/task2.js
@@ -0,0 +1,19 @@
+//================ Задача 2
+
+// Користувач вводить кількість елементів. Створити масив, у якому перша половина заповнена 1-цями, а друга заповнена 7-ками.
+
+const fillArray1_7 = () => {
+  const elementsAmount = parseInt(document.getElementById('task2').value)
+  // умова перевіряє чи не пусте значення в інпуті
+  if (elementsAmount) {
+    let resultArr = new Array(elementsAmount).fill(1)
+    const halfOfElements = Math.floor(elementsAmount / 2)
+    resultArr = resultArr.map((it, index) => {
+      if (index >= halfOfElements) it = 7
+      return it
+    })
+    document.querySelector('.result_2').innerHTML = `
+      ${resultArr}
+    `
+  }
+}
