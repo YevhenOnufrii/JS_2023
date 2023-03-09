25fbd083be346b7fa02a1f884531571d0c7ae368
Yevhen <onufrii.socnet@gmail.com>
2023-03-09 23:42:23 +0200

    add template HW#10

--- Lesson_9/js/task10.js
+++ Lesson_9/js/task10.js
@@ -8,11 +8,11 @@ function getArrayOfTaxesPaid() {
     return Math.floor(Math.random() * (max - min + 1)) + min
   }
   // масив цін
-  const pricesArr = new Array(20).fill(null).map((it) => (it = getRandomNumb(100, 2000)))
+  const pricesArr = new Array(20).fill(null).map(it => (it = getRandomNumb(100, 2000)))
   // розрахунок суми податку від ціни товару
   const calculateTax = (taxRate, itemPrice) => (itemPrice * taxRate) / 100
   // масив із величин сплачених податків
-  const resultArr = pricesArr.map((it) => (it = calculateTax(TAX_RATE, it)))
+  const resultArr = pricesArr.map(it => (it = calculateTax(TAX_RATE, it)))
   // сума сплачених податків
   let amountOfTaxesPaid = resultArr.reduce((acc, it) => (acc += it), 0)
   //вивід результатів
