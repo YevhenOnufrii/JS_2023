ce702418c530e543835d52e49bc812c6df954449
Yevhen <onufrii.socnet@gmail.com>
2023-03-10 14:43:02 +0200

    add extra task 2 HW#10

--- Lesson_10/js/extraTasks.js
+++ Lesson_10/js/extraTasks.js
@@ -3,3 +3,7 @@
 const someArr = new Array(10)
   .fill(0)
   .map((num, index, arr) => (index === 0 || index === arr.length - 1 ? (num = 1) : num))
+
+// 2. Заполнить массив нулями и единицами, при этом данные значения чередуются, начиная с нуля.
+
+const someArr2 = new Array(10).fill(null).map((numb, i) => (i % 2 ? (numb = 1) : (numb = 0)))
