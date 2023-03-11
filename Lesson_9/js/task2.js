4f29d04930d59334a9e1562370292bf863fa047c
Yevhen <onufrii.socnet@gmail.com>
2023-03-12 00:28:53 +0200

    fix task2 HW9

--- Lesson_9/js/task2.js
+++ Lesson_9/js/task2.js
@@ -6,6 +6,16 @@ const fillArray1_7 = () => {
   const elementsAmount = parseInt(document.getElementById('task2').value)
   // умова перевіряє чи не пусте значення в інпуті
   if (elementsAmount) {
+    let resultArr = new Array(elementsAmount).fill(1).fill(7, Math.floor(elementsAmount / 2))
+    document.querySelector('.result_2').innerHTML = `
+    ${resultArr}
+    `
+  }
+}
+/* const fillArray1_7 = () => {
+  const elementsAmount = parseInt(document.getElementById('task2').value)
+  // умова перевіряє чи не пусте значення в інпуті
+  if (elementsAmount) {
     let resultArr = new Array(elementsAmount).fill(1)
     const halfOfElements = Math.floor(elementsAmount / 2)
     resultArr = resultArr.map((it, index) => {
@@ -16,4 +26,4 @@ const fillArray1_7 = () => {
       ${resultArr}
     `
   }
-}
+} */
